18db19507436b1a356a944235ce1244bb6714f9a
Charapao <pao_tia@hotmail.com>
2014-11-17 10:51:21 +0700

    moiving ship

--- src/GameLayer.js
+++ src/GameLayer.js
@@ -1,17 +1,13 @@
-// var Ship = cc.Sprite.extend({
-//     ctor: function() {
-//         this._super();
-//         this.initWithFile( 'images/ship.png' );
-//     }
-// });
-
 var GameLayer = cc.LayerColor.extend({
     init: function() {
         this._super( new cc.Color4B( 127, 127, 127, 255 ) );
         this.setPosition( new cc.Point( 0, 0 ) );
+ 
         var ship = new Ship();
-        ship.setPosition( new cc.Point( 200, 200 ) );
+        ship.setPosition( new cc.Point( 200, 220 ) );
         this.addChild( ship );
+        ship.scheduleUpdate();
+     
         return true;
     }
 });
